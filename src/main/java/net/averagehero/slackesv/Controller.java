50967010c02cfbedcc03b437394f5c25b19c5e4d
Jon Hughes <jon.hughes@vitalize.com>
2017-12-12 08:53:08 -0800

    Doh, extraneous close. The compiler adds it for me, no need for explicit

--- src/main/java/net/averagehero/slackesv/Controller.java
+++ src/main/java/net/averagehero/slackesv/Controller.java
@@ -126,8 +126,6 @@ public class Controller {
                 try (Response esvResponse = client.newCall(esvRequest).execute()) {
                     logger.debug("ESV Request: " + (System.currentTimeMillis() - start) + " ms.");
 
-                    esvResponse.close();
-
                     //body = "{\"query\":\"Genesis 1:1,John 1:1\",\"canonical\":\"Genesis 1:1; John 1:1\",\"parsed\":[[1001001,1001001],[43001001,43001001]],\"passage_meta\":[{\"canonical\":\"Genesis 1:1\",\"chapter_start\":[1001001,1001031],\"chapter_end\":[1001001,1001031],\"prev_verse\":null,\"next_verse\":1001002,\"prev_chapter\":null,\"next_chapter\":[1002001,1002025]},{\"canonical\":\"John 1:1\",\"chapter_start\":[43001001,43001051],\"chapter_end\":[43001001,43001051],\"prev_verse\":42024053,\"next_verse\":43001002,\"prev_chapter\":[42024001,42024053],\"next_chapter\":[43002001,43002025]}],\"passages\":[\"\\nGenesis 1:1\\n\\n\\nThe Creation of the World\\n\\n  [1] In the beginning, God created the heavens and the earth. (ESV)\",\"\\nJohn 1:1\\n\\n\\nThe Word Became Flesh\\n\\n  [1] In the beginning was the Word, and the Word was with God, and the Word was God. (ESV)\"]}";
                     body = esvResponse.body().string();
                     if (esvResponse.isSuccessful()) {
